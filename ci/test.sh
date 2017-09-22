58983dde560adb6ad98a58f465a4087a02ad78ae
jirapat <jirapat_tanghongs@homedepot.com>
2017-09-22 09:33:05 -0500

    setup concourse pipeline

---
+++ ci/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e -x
+
+pwd
+
+pushd flight-school
+  env
+  curl http://services.faa.gov/airport/status/DFW?format=application/json
+  bundle install
+  bundle exec rspec
+popd
